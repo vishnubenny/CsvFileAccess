dab0b16da3f7085c2e55c992d88c0d3890a12c73
Vishnu <vishnu@vishnu.(none)>
2017-02-06 09:31:49 +0530

    Tested setting up of project in another system and testing commit Resources handling through methods are started integrating in mainactivity

--- app/src/main/java/lv/com/csvfileaccess/view/activity/MainActivity.java
+++ app/src/main/java/lv/com/csvfileaccess/view/activity/MainActivity.java
@@ -1,17 +1,13 @@
 package lv.com.csvfileaccess.view.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 import lv.com.csvfileaccess.R;
@@ -20,12 +16,22 @@ import lv.com.csvfileaccess.model.pojo.RowData;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        findResources();
+        initializeResources();
+    }
+
+    private void initializeResources() {
         setSupportActionBar(toolbar);
+    }
+
+    private void findResources() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
     }
 
     @Override
